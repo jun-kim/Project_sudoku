f9a16ed8c48f05e4b87728941b5797d21e4ef95c
Poo123 <kimdanwoo@gmail.com>
2023-09-26 21:20:54 +1000

    hih

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,14 @@
 def print_menu():
   print("Poo")
-  
+
+def grab(sudoku):
+  rows = [p for p in sudoku.split("\n")]
+  columns = []
+  boxes = []
+  return rows
+
 print("sudoku is here")
 print_menu()
+
+sudoku = input("Sudoku (mark unknown with X): ")
+print(grab("12345678"))
