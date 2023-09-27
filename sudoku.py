e657ea263b2ee7dda1c45afb768c1b8f040fae45
Poo123 <kimdanwoo@gmail.com>
2023-09-27 12:12:15 +1000

    Number 3 done

--- sudoku.py
+++ sudoku.py
@@ -1,8 +1,18 @@
-def print_menu():
-  print("Poo")
+def print_menu(sudoku):
+  final = [[]]
+  counter = 0
+  for i in sudoku:
+    if counter == 9:
+      final.append([])
+      counter = 0
+    final[-1].append(i)
+    counter += 1
+  return final
 
-def change(x,y,n):
-  pass
+def change(x,y,n,sudoku):
+  newsudoku = print_menu(sudoku)
+  newsudoku[y][x] = n
+  return newsudoku
 
 def grab(sudoku):
   rows = []
@@ -11,7 +21,6 @@ def grab(sudoku):
   return rows
 
 print("sudoku is here")
-print_menu()
 
 sudoku = input("Sudoku (mark unknown with X): ")
 print(grab("12345678"))
@@ -22,3 +31,5 @@ print(grab("12345678"))
 #                                         987 654 321 
 #3 . change the array value using the menu. So use the menu to update a value. input 0 0 3 makes 1 -> 3
 #4 . print menu prints updated sudoku array (regardless of correct or not)
+
+print(change(int(input("x:")),int(input("y:")),input("n:"),input("sudoku:")))
