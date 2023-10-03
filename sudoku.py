389603d84df480ea2d0a430135569fcb6a6fe5fc
Poo123 <kimdanwoo@gmail.com>
2023-10-03 16:30:52 +1100

    Worked on it a little

--- sudoku.py
+++ sudoku.py
@@ -1,16 +1,18 @@
-def print_menu(sudoku): # turns 
-  final = [[]]
-  counter = 0
-  for i in sudoku:
+def read(sudoku): # turns sudoku readable (into rows)
+  final = []
+  counter = 9
+  for num in sudoku.replace(" ",""):
     if counter == 9:
       final.append([])
       counter = 0
-    final[-1].append(i)
+    final[-1].append(num)
     counter += 1
+  # after tests implement if statement to measure length of final
   return final
 
+# 3
 def change(x,y,n,sudoku):
-  newsudoku = print_menu(sudoku)
+  newsudoku = read(sudoku)
   newsudoku[y][x] = n
   return newsudoku
 
@@ -36,6 +38,7 @@ def solve(sudoku, x=0, y=0): # x and y are coordinates; they start as zero, but 
         sudoku[x][y] = 0 # undo the cell to try again with a new value
   return False
 
+# 2
 def format(sudoku): # sudoku variable is the solved sudoku which is a list of lists (rows)
   final = ""
   counter = 0
@@ -54,12 +57,34 @@ def format(sudoku): # sudoku variable is the solved sudoku which is a list of li
 def convert():
   pass # use this function to turn sudoku input into something readable by solve function
 
+# 1
 sudoku = input("SUDOKU\nInput conditions:\n1) Start from the top left cell, moving right\n2) Use a space to represent the end of a row\n3) Indicate unknown values with a 0\nExample: 120456709 987600321...\nSudoku: ")
 # use this sudoku format ^
+"1) initial array - numbers with x for empty cell. ex) 1 12345678x"
+"2) display the array. Ex) 2 then display 123 | 456 | 78x"
+"3) enter a number in the array. Ex) 3 0 0 5. Then display will show 523 | 456 | 78x"
 
+def print_menu(cmd,sudoku):
+  if cmd == "1":
+    return format(sudoku)
+  elif cmd == "2":
+    return change(int(input("x: ")),int(input("y: ")),int(input("n: ")),sudoku)
+
+def display(sudoku):
+  counter = 3
+  sudoku = read(sudoku)
+  for row in sudoku:
+    if counter == 3:
+      print("----------------")
+      counter = 0
+    print("{}{}{} | {}{}{} | {}{}{}".format(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8]))
+    counter += 1
+
+display(sudoku)
 #instruction:
 #1 . print the menu for sudoku user interface
 #2 . print out a simple sudoku array. EX) 323 456 789
 #                                         987 654 321 
 #3 . change the array value using the menu. So use the menu to update a value. input 0 0 3 makes 1 -> 3
 #4 . print menu prints updated sudoku array (regardless of correct or not)
+
